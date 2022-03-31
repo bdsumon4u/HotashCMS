8e039b7b6dfa93eea852d066e633dcf7430b65e2
Sumon Ahmed <bdsumon4u@gmail.com>
2022-03-31 20:29:49 +0600

    Tenant Admin

--- routes/tenant.php
+++ routes/tenant.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -14,6 +15,17 @@ use Inertia\Inertia;
 | Feel free to customize them however you want. Good luck!
 |
 */
+
+Route::domain('admin.{domain}')->as('admin.')->group(function () {
+    Route::redirect('/', RouteServiceProvider::HOME);
+    Route::redirect('/register', RouteServiceProvider::HOME);
+
+    Route::middleware(['auth:admin', 'verified'])->group(function () {
+        Route::get('/dashboard', function () {
+            return Inertia::render('packs/admin::Dashboard');
+        })->name('dashboard');
+    });
+});
 
 Route::get('/', function () {
     return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
